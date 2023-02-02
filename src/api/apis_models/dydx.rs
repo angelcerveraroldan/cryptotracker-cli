c6155eea87f151df5de655983f2c7bde8ea25feb
angelcerveraroldan <48255007+angelcerveraroldan@users.noreply.github.com>
2023-02-02 14:18:00 +0000

    Get candles for DyDx and for mexc

--- src/api/apis_models/dydx.rs
+++ src/api/apis_models/dydx.rs
@@ -1,6 +1,6 @@
 use std::collections::HashMap;
 use serde::{Deserialize, Deserializer};
-use crate::api::SymbolsReply;
+use crate::api::{Candle, CandleReply, SymbolsReply};
 
 #[derive(Deserialize, Debug)]
 pub struct DyDxSymbolsReply {
@@ -24,5 +24,41 @@ impl SymbolsReply for DyDxSymbolsReply {
         }
 
         symbols
+    }
+}
+
+#[derive(Deserialize, Debug, Clone)]
+#[serde(rename_all = "camelCase")]
+pub struct DyDxCandleReply {
+    started_at: String,
+    resolution: String,
+    low: String,
+    high: String,
+    open: String,
+    close: String,
+    usd_volume: String,
+}
+
+#[derive(Deserialize, Debug)]
+pub struct DyDxCandlesReply {
+    candles: Vec<DyDxCandleReply>,
+}
+
+impl CandleReply for DyDxCandlesReply {
+    fn get_candles_arr(&self) -> Vec<Candle> {
+        let mut candles: Vec<Candle> = Vec::new();
+
+        for c in self.candles.clone().into_iter() {
+            candles.push(Candle {
+                timestamp: 0,
+                open: c.open.parse::<f32>().unwrap(),
+                close: c.close.parse::<f32>().unwrap(),
+                high: c.high.parse::<f32>().unwrap(),
+                low: c.low.parse::<f32>().unwrap(),
+                volume: c.usd_volume.parse::<f32>().ok(),
+            })
+        }
+
+        candles
     }
 }
--- src/api/apis_models/mexc.rs
+++ src/api/apis_models/mexc.rs
@@ -1,4 +1,4 @@
-use crate::api::{Api, Candle, SymbolsReply};
+use crate::api::{Api, Candle, CandleReply, SymbolsReply};
 use serde::{Deserialize, Deserializer};
 
 #[derive(Deserialize, Debug)]
@@ -22,5 +22,41 @@ impl SymbolsReply for MexcSymbolsReply {
         }
 
         symbols
+    }
+}
+
+#[derive(Debug, Clone, Deserialize)]
+pub struct MexcCandleReply {
+    timestamp: u32,
+    open: String,
+    close: String,
+    high: String,
+    low: String,
+    volume: String,
+    amount: String,
+}
+
+#[derive(Debug, Deserialize)]
+pub struct MexcCandlesReply {
+    code: u8,
+    data: Vec<MexcCandleReply>,
+}
+
+impl CandleReply for MexcCandlesReply {
+    fn get_candles_arr(&self) -> Vec<Candle> {
+        let mut candles: Vec<Candle> = Vec::new();
+
+        for c in self.data.clone().into_iter() {
+            candles.push(Candle {
+                timestamp: c.timestamp,
+                open: c.open.parse::<f32>().unwrap(),
+                close: c.close.parse::<f32>().unwrap(),
+                high: c.high.parse::<f32>().unwrap(),
+                low: c.low.parse::<f32>().unwrap(),
+                volume: c.volume.parse::<f32>().ok(),
+            })
+        }
+
+        candles
     }
 }
